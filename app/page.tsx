3dbc6f338d00517c1acd0ff4d161d2e17fe59b52
Austyn Studdard <KingCosmicDev@gmail.com>
2024-03-18 22:08:56 -0500

    udpated blog.

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,6 @@ import {
   getViewsCount,
 } from 'app/db/queries';
 
-function Badge(props) {
-  return (
-    <a
-      {...props}
-      target="_blank"
-      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline"
-    />
-  );
-}
-
 function ArrowIcon() {
   return (
     <svg
@@ -42,64 +32,6 @@ function ArrowIcon() {
         fill="currentColor"
       />
     </svg>
-  );
-}
-
-function ChannelLink({ img, link, name }) {
-  return (
-    <div className="group flex w-full">
-      <a
-        href={link}
-        target="_blank"
-        className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
-      >
-        <div className="flex items-center space-x-3">
-          <div className="relative h-16">
-            <Image
-              alt={name}
-              src={img}
-              height={64}
-              width={64}
-              sizes="33vw"
-              className="border border-neutral-200 dark:border-neutral-700 rounded-full h-16 w-16"
-              priority
-            />
-            <div className="border border-neutral-200 dark:border-neutral-700 rounded-full bg-white inline-flex p-1 relative h-6 w-6 items-center -top-6 -right-10">
-              <svg width="15" height="11" role="img" aria-label="YouTube logo">
-                <use href="/sprite.svg#youtube" />
-              </svg>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <p className="font-medium text-neutral-900 dark:text-neutral-100">
-              {name}
-            </p>
-            <Suspense fallback={<p className="h-6" />}>
-              <Subs name={name} />
-            </Suspense>
-          </div>
-        </div>
-        <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
-          <ArrowIcon />
-        </div>
-      </a>
-    </div>
-  );
-}
-
-async function Subs({ name }: { name: string }) {
-  noStore();
-  let subscribers;
-  if (name === '@leerob') {
-    subscribers = await getLeeYouTubeSubs();
-  } else {
-    subscribers = await getVercelYouTubeSubs();
-  }
-
-  return (
-    <p className="text-neutral-600 dark:text-neutral-400">
-      {subscribers} subscribers
-    </p>
   );
 }
 
@@ -137,7 +69,7 @@ function ProjectLink({ name, description, link }) {
           <p className="font-medium text-neutral-900 dark:text-neutral-100">
             {name}
           </p>
-          <p className="h-6 text-neutral-600 dark:text-neutral-400">
+          <p className="text-lg text-neutral-600 dark:text-neutral-400">
             {description}
           </p>
         </div>
@@ -167,69 +99,6 @@ export default function Page() {
         {` as a self employed Software Engineer.`}
       </p>
 
-      {/* <div className="columns-2 sm:columns-3 gap-4 my-8">
-        <div className="relative h-40 mb-4">
-          <Image
-            alt="Me speaking on stage at React Summit about the future of Next.js"
-            src={summit}
-            fill
-            sizes="(max-width: 768px) 213px, 33vw"
-            priority
-            className="rounded-lg object-cover"
-          />
-        </div>
-        <div className="relative h-80 mb-4 sm:mb-0">
-          <Image
-            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
-            src={filming}
-            fill
-            sizes="(max-width: 768px) 213px, 33vw"
-            priority
-            className="rounded-lg object-cover object-[-16px] sm:object-center"
-          />
-        </div>
-        <div className="relative h-40 sm:h-80 sm:mb-4">
-          <Image
-            alt="Me standing on stage at Reactathon delivering the keynote"
-            src={reactathon}
-            fill
-            sizes="(max-width: 768px) 213px, 33vw"
-            priority
-            className="rounded-lg object-cover object-top sm:object-center"
-          />
-        </div>
-        <div className="relative h-40 mb-4 sm:mb-0">
-          <Image
-            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
-            src={smashing}
-            fill
-            sizes="(max-width: 768px) 213px, 33vw"
-            priority
-            className="rounded-lg object-cover"
-          />
-        </div>
-        <div className="relative h-40 mb-4">
-          <Image
-            alt="Me and Guillermo Rauch on stage for Vercel Ship, answering questions from the Next.js community"
-            src={ship}
-            fill
-            sizes="(max-width: 768px) 213px, 33vw"
-            priority
-            className="rounded-lg object-cover"
-          />
-        </div>
-        <div className="relative h-80">
-          <Image
-            alt="My badge on top of a pile of badges from a Vercel meetup we held"
-            src={meetups}
-            fill
-            sizes="(min-width: 768px) 213px, 33vw"
-            priority
-            className="rounded-lg object-cover"
-          />
-        </div>
-      </div> */}
-
       <div className="prose prose-neutral dark:prose-invert">
         <p>
           I contribute to open-source projects such as Phaser.js, Fastifyjs,
@@ -239,19 +108,6 @@ export default function Page() {
           in the tech industry.
         </p>
       </div>
-  
-      {/* <div className="my-8 flex flex-col sm:flex-row space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 w-full">
-        <ChannelLink
-          img={avatar}
-          name="@leerob"
-          link="https://www.youtube.com/@leerob"
-        />
-        <ChannelLink
-          img={vercel}
-          name="@vercel"
-          link="https://www.youtube.com/@vercelhq"
-        />
-      </div> */}
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
@@ -272,7 +128,7 @@ export default function Page() {
         />
       </div>
 
-      <div className="prose prose-neutral dark:prose-invert">
+      {/* <div className="prose prose-neutral dark:prose-invert">
         <p>
           and here you'll find writing about technologies
           I'm interested in at the time, or how I'm learning and growing in my
@@ -282,14 +138,13 @@ export default function Page() {
 
       <div className="my-8 flex flex-col space-y-4 w-full">
         <BlogLink
-          name="What Makes A Great Developer Experience?"
-          slug="developer-experience-examples"
+          name="Exploring Game Development with Phaser.js"
+          slug="exploring-game-development-with-phaser"
         />
-        <BlogLink name="What is Developer Relations?" slug="devrel-at-vercel" />
-      </div>
+      </div> */}
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          Tech I'm excited about.
+          And here you can find some Tech i'm excited about.
         </p>
       </div>
       <div className="my-8 flex flex-row space-x-2 w-full h-14 overflow-x-auto">
